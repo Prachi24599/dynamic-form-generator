6bef1b4d45d58a28596d430b3e348d2d34decd4e
Prachi24599 <prachi24599@gmail.com>
2023-07-07 11:58:25 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Form from "./components/Input";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
