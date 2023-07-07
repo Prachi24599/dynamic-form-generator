c0d2c293ec903b68fb068eb54bd88bdcd5062bc2
Prachi24599 <prachi24599@gmail.com>
2023-07-07 12:03:08 +0530

    basic form structure created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Form from "./components/Input";
+import Form from "./components/Form";
 
 function App() {
   return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div>
+      <form>
+        <div className="input-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <button type="submit" className="submit-btn">
+          Sumbit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
