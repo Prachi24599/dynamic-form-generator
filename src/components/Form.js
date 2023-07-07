702c4bb6f60081533ec5808b8a908b26980bed90
Prachi24599 <prachi24599@gmail.com>
2023-07-07 12:34:31 +0530

    state attahced to each form elememt

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 const Form = () => {
+  const [formValues, setFormValues] = useState({});
+
+  function handleChange(e) {
+    setFormValues((prevFormValues) => {
+      return { ...prevFormValues, [e.target.id]: e.target.value };
+    });
+  }
+
+  function handleSubmit(e) {
+    e.prevFormValues();
+    console.log(formValues);
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-group">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={formValues.name || " "}
+            onChange={handleChange}
+          />
         </div>
         <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            id="email"
+            value={formValues.email || ""}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit" className="submit-btn">
           Sumbit
